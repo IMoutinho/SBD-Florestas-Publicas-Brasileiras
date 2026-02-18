dc412963d5627b5a021a44f84841f7a96a5350c8
Gabriel Amorim <gabrielo.hca@gmail.com>
2026-02-18 16:39:35 -0300

    sugestoes

--- sql/consultas.sql
+++ sql/consultas.sql
@@ -1,2 +1,27 @@
 -- Arquivo para consultas SQL
 -- Comandos SELECT
+
+
+-- Relatório de unidades listando nome, ato legal e ano de criação.
+
+-- Quantas florestas não foram regulamentadas na última década?
+
+-- Quais unidades estão em estágio "Crítico" (ex: 'Em estudo' há muito tempo)?
+
+-- Quantas Unidades de Conservação existem por Bioma?
+
+-- Contagem de unidades agrupadas por ano de criação.
+
+-- Quantidade de unidades e área total florestal (soma de hectares) por Estado (UF).
+
+-- Quantas florestas cada bioma tem, separado por tipo de proteção.
+
+-- Total de áreas geridas por órgão governamental (agrupado por nome do órgão e estado).
+
+-- Quais Biomas possuem a maior média de área por unidade?
+
+-- Percentual de área total ocupada por cada tipo de proteção em relação ao todo.
+
+-- Qual é a unidade com a maior área absoluta registrada no banco de dados?
+
+-- Liste as unidades que são maiores que a média de tamanho de todas as unidades do seu próprio bioma.
