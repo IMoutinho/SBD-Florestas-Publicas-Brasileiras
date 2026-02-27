d9e1f3c42f735d28cf367858a1e3099846bafd13
Gabriel Moutinho <146742915+IMoutinho@users.noreply.github.com>
2026-02-27 17:08:38 -0300

    arquivo para elaboração de procedimentos e gatilhos

---
+++ sql/procedimentos_gatilhos.sql
@@ -0,0 +1,22 @@
+-- PROCEDIMENTOS E GATILHO
+
+CREATE OR REPLACE FUNCTION verificarEstagioCritico(unidade_id TEXT)
+RETURNS TEXT AS $$
+DECLARE
+    v_nome TEXT;
+    v_estagio TEXT;
+    v_area NUMERIC;
+    v_ano INT;
+BEGIN
+    SELECT n.nome, u.estagio, m.area_ha, EXTRACT(YEAR FROM d.data_criacao)
+    INTO v_nome, v_estagio, v_area, v_ano
+    FROM Unidade u, Nome_Unidade n, Metricas_Area m, Data_Criacao_Unidade d 
+   WHERE  u.codigo = n.unidade_codigo AND u.area_ha = m.area_ha AND u.codigo = d.unidade_codigo AND u.codigo = unidade_id;
+    IF v_ano > 2000 AND v_area > 100000 AND v_estagio IN ('Em estudo', 'Delimitada', 'Declarada') THEN
+        RETURN 'ALERTA: A Unidade ' || v_nome || ' possui vasta extensão (' || v_area || ' ha) e permanece em estágio inicial de regularização.';
+    ELSE
+        RETURN 'Status Regular: A unidade não apresenta inconsistências de maturação territorial.';
+    END IF;
+
+END $$
+LANGUAGE plpgsql;
